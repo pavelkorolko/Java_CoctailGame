3f4260661cb9ba399714bd73fa12464dc487996d
Pavel <pavelkorolko@gmail.com>
2024-09-15 17:13:13 +0300

    amendmends in first commit

---
+++ src/main/java/Models/Helper.java
@@ -0,0 +1,13 @@
+package Models;
+
+public final  class Helper {
+
+    public static boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
